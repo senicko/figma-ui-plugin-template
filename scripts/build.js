e94d582fcfeb3451d6a16de3b63d37175885f729
SeNicko <sebastianflajszer@gmail.com>
2022-08-18 23:01:16 +0200

    Bundle js and css into single html file

--- scripts/build.js
+++ scripts/build.js
@@ -1,35 +1,53 @@
 const esbuild = require("esbuild");
-const fs = require("fs");
+const fs = require("fs/promises");
+const { name } = require("../manifest.json");
+const { minify } = require("html-minifier");
 
-fs.rm("./build", { recursive: true, force: true }, (err) => {
-  if (err) {
-    console.error("failed to clear build directory", err);
-    process.exit(1);
-  }
+const build = async () => {
+  await fs.rm("./build", { recursive: true, force: true });
 
-  // build plugin
-  esbuild
-    .build({
-      entryPoints: ["./plugin/plugin.ts"],
-      bundle: true,
-      minify: true,
-      outfile: "./build/plugin.js",
+  await esbuild.build({
+    entryPoints: ["./plugin/plugin.ts"],
+    bundle: true,
+    minify: true,
+    outfile: "./build/plugin.js",
+  });
+
+  const result = await esbuild.build({
+    entryPoints: ["./ui/main.tsx"],
+    bundle: true,
+    minify: true,
+    write: false,
+    outdir: "./build",
+  });
+
+  let html = (await fs.readFile("./ui/template.html")).toString();
+
+  html = html.replaceAll("__name__", name);
+
+  const headClosing = html.indexOf("</head>");
+  const bodyClosing = html.indexOf("</body>");
+
+  const js = result.outputFiles.find((file) => file.path.endsWith(".js"));
+  const css = result.outputFiles.find((file) => file.path.endsWith(".css"));
+
+  html =
+    html.slice(0, headClosing) +
+    `<style>${css.text}</style>` +
+    html.slice(headClosing, bodyClosing) +
+    `<script>${js.text}</script>` +
+    html.slice(bodyClosing);
+
+  await fs.writeFile(
+    "./build/ui.html",
+    minify(html, {
+      removeComments: true,
+      collapseWhitespace: true,
     })
-    .catch(() => process.exit(1));
+  );
+};
 
-  // TODO: Bundle ui.js and it's css file into single ui.html file
-
-  // build ui
-  esbuild
-    .build({
-      entryPoints: ["./ui/main.tsx"],
-      bundle: true,
-      minify: true,
-      write: false,
-      outfile: "./build/ui.js",
-    })
-    .catch(() => process.exit(1))
-    .then((result) => {
-      console.log(result.outputFiles);
-    });
+build().catch((err) => {
+  console.error("Build failed", err);
+  process.exit(1);
 });
